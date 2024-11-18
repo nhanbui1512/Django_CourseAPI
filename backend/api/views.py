85ea494257acd5d0ac4e630c87ff53c4b1555a01
Nhan Bui <buithiennhan0345@gmail.com>
2024-11-18 10:23:53 +0700

    initial source

---
+++ backend/api/views.py
@@ -0,0 +1,16 @@
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from product.models import Product
+
+
+
+# Create your views here.
+@api_view(["GET"])
+def api_home(request, *args,**kwargs):
+  model_data = Product.objects.all().order_by("?").first()
+  data = {}
+  if(model_data):
+    data = model_to_dict(model_data, fields=['title','id'])
+  return Response(data)
---
+++ py_client/basic.py
@@ -0,0 +1,5 @@
+import requests
+
+endpoint = 'https://httpbin.org/anything'
+
+requests.get(endpoint)
